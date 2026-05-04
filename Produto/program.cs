a93e6852f776b90e62850d86e03c95fd7d379f10
juliamine969-jpg <juliamine969@gmail.com>
2026-05-04 14:49:24 -0300

    Update program.cs

--- Produto/program.cs
+++ Produto/program.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace joaomaio
+{
+    internal class Produto
+    {
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos (int quantidade)
+        { Quantidade -= quantidade;}
+           
+        public override string ToString() => Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + "unidades,Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+         }
+    }
