5b1313da8d27542cac24ea146a539147dac47ac5
juliamine969-jpg <juliamine969@gmail.com>
2026-05-04 14:58:16 -0300

    Update Program.cs

--- Produto/Program.cs
+++ Produto/Program.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine(Entre com os dados do Produto");
+            Console.Write("Nome: ")
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados do produto: "+p)
+
+            Console.WriteLine("Digite o numero de produtos a ser adicionado do estoque");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+    }
+}
 
